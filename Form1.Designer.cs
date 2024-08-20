5e4e323707dbce874a6fc4a84b5b986047acc684
Apple OhSeed <appleohseed@gmail.com>
2024-08-20 21:45:59 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsAppdll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstNo = new System.Windows.Forms.TextBox();
+            this.txtSecNo = new System.Windows.Forms.TextBox();
+            this.ADDbutton = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SUBbutton = new System.Windows.Forms.Button();
+            this.MULbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstNo
+            // 
+            this.txtFirstNo.Location = new System.Drawing.Point(23, 25);
+            this.txtFirstNo.Name = "txtFirstNo";
+            this.txtFirstNo.Size = new System.Drawing.Size(74, 20);
+            this.txtFirstNo.TabIndex = 0;
+            // 
+            // txtSecNo
+            // 
+            this.txtSecNo.Location = new System.Drawing.Point(103, 25);
+            this.txtSecNo.Name = "txtSecNo";
+            this.txtSecNo.Size = new System.Drawing.Size(79, 20);
+            this.txtSecNo.TabIndex = 1;
+            // 
+            // ADDbutton
+            // 
+            this.ADDbutton.Location = new System.Drawing.Point(23, 73);
+            this.ADDbutton.Name = "ADDbutton";
+            this.ADDbutton.Size = new System.Drawing.Size(42, 32);
+            this.ADDbutton.TabIndex = 2;
+            this.ADDbutton.Text = "+";
+            this.ADDbutton.UseVisualStyleBackColor = true;
+            this.ADDbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(46, 130);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(118, 20);
+            this.txtResult.TabIndex = 3;
+            // 
+            // SUBbutton
+            // 
+            this.SUBbutton.Location = new System.Drawing.Point(82, 75);
+            this.SUBbutton.Name = "SUBbutton";
+            this.SUBbutton.Size = new System.Drawing.Size(39, 30);
+            this.SUBbutton.TabIndex = 4;
+            this.SUBbutton.Text = "-";
+            this.SUBbutton.UseVisualStyleBackColor = true;
+            this.SUBbutton.Click += new System.EventHandler(this.SUBbutton_Click);
+            // 
+            // MULbutton
+            // 
+            this.MULbutton.Location = new System.Drawing.Point(144, 75);
+            this.MULbutton.Name = "MULbutton";
+            this.MULbutton.Size = new System.Drawing.Size(38, 28);
+            this.MULbutton.TabIndex = 5;
+            this.MULbutton.Text = "x";
+            this.MULbutton.UseVisualStyleBackColor = true;
+            this.MULbutton.Click += new System.EventHandler(this.MULbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 192);
+            this.Controls.Add(this.MULbutton);
+            this.Controls.Add(this.SUBbutton);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.ADDbutton);
+            this.Controls.Add(this.txtSecNo);
+            this.Controls.Add(this.txtFirstNo);
+            this.Name = "Form1";
+            this.Text = "+ / - / x   via DLL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstNo;
+        private System.Windows.Forms.TextBox txtSecNo;
+        private System.Windows.Forms.Button ADDbutton;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button SUBbutton;
+        private System.Windows.Forms.Button MULbutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculatedll;
+
+namespace WindowsFormsAppdll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Calculate cal = new Calculate();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //storing the result in int i
+                int i = cal.Add(int.Parse(txtFirstNo.Text), int.Parse(txtSecNo.Text));
+                txtResult.Text = i.ToString();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SUBbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //storing the result in int i
+                int i = cal.Sub(int.Parse(txtFirstNo.Text), int.Parse(txtSecNo.Text));
+                txtResult.Text = i.ToString();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MULbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //storing the result in int i
+                int i = cal.Mul(int.Parse(txtFirstNo.Text), int.Parse(txtSecNo.Text));
+                txtResult.Text = i.ToString();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
